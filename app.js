7afd402acdee490892c0e905d0789d59e7fd4818
Luiz Henrique <58668840+luiz-tm@users.noreply.github.com>
2022-08-13 02:07:25 -0300

    clean code + organização +  ui design responsivo

--- app.js
+++ app.js
@@ -1,66 +1,93 @@
+// Document Object Model
+const timer = document.querySelector('#timer')
+const start = document.querySelector('#start')
+const pause = document.querySelector('#pause')
+const clear = document.querySelector('#clear')
+const pauseStatus = document.querySelector('#pause-status')
+//
+
+// Application Settings
+const defaultTimer = 1000;
+let timerUpdate, running = false;
+let  hour = 0, minute = 0, second = 0;
+//
+
+// Time GET and SET methods
+const setHour = (value) => { hour = value; }
+const setMinute = (value) => { minute = value; }
+const setSecond = (value) => { second = value; }
+
+const getHour = () => hour;
+const getMinute = () => minute;
+const getSecond = () => second;
+//
+
+// Stopwatch running status
+const setRunningStatus = (value = false) => running = value;
+const getRunningStatus = () => running;
+//
+
+// Toggle pause status (show and hide methods)
+const showPauseStatus = () => pauseStatus.classList.add('active')
+const hidePauseStatus = () => pauseStatus.classList.remove('active')
+//
+
+// Pad with a leading zero
+const fixNumber = (number) => number < 10 ? `0${number}` : number;
+//
+
+// Update timer HTML element
+const updateTimer = () => timer.innerHTML = `${fixNumber(hour)}:${fixNumber(minute)}:${fixNumber(second)}`
+//
+
+// Application
 const App = () =>
 {
-    const timer = document.querySelector('#timer')
-    const start = document.querySelector('#start')
-    const pause = document.querySelector('#pause')
-    const clear = document.querySelector('#clear')
-    const pauseStatus = document.querySelector('#pause-status')
-    let timerUpdate, running = false;
-    let  hour = 0, minute = 0, second = 0;
-
-    const fixNumber = (number) => 
-    {
-        let FixedNumber = number < 10 ? `0${number}` : number;
-        return FixedNumber;
-    }
-
-    const updateTimer = () => timer.innerHTML = `${fixNumber(hour)}:${fixNumber(minute)}:${fixNumber(second)}`
-
-    const stopApp = (zero = false) =>
-    {
-        switch(zero)
-        {
-            case true:
-                hour = 0, minute = 0, second = 0 
-                pauseStatus.classList.remove('active')
-                break;
-
-            case false:
-                pauseStatus.classList.add('active')
-                break;
-        }
-        running = false;
-        clearInterval(timerUpdate)
-        updateTimer()
-    }
-
     pause.addEventListener('click', () => stopApp() )
     clear.addEventListener('click', () => stopApp(true) )
-    
+
     start.addEventListener('click', () => {
-        if(running === false)
+        if(getRunningStatus() === false)
         {
             timerUpdate = setInterval(() => {
-                second++;
+                setSecond(getSecond() + 1);
         
-                if(second === 60)
+                if(getSecond() === 60)
                 {
-                    second = 0;
-                    if(minute < 59) { minute++; }
+                    setSecond(0);
+                    if(getMinute() < 59) { setMinute(minute + 1); }
                     else 
                     {
-                        hour++;
-                        minute = 0;
+                        setHour(getHour() + 1);
+                        setMinute(0);
                     }
                 }
                 
                 updateTimer();
-            }, 1000)
-            running = true;
-            pauseStatus.classList.remove('active')
+            }, defaultTimer)
+            setRunningStatus(true);
+            hidePauseStatus()
         }
     })
-    
+
+    const stopApp = (clearButton = false) =>
+    {
+        switch(clearButton)
+        {
+            case true:
+                setHour(0), setMinute(0), setSecond(0) 
+                hidePauseStatus()
+                break;
+
+            case false:
+                showPauseStatus()
+                break;
+        }
+        setRunningStatus(false);
+        clearInterval(timerUpdate)
+        updateTimer()
+    }
 }
+//
 
 App();
